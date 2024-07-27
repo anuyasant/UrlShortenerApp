c717db1036d5f94230bae1d8ba5b171ca52c25fa
Anuya Sant <sant.anuya@gmail.com>
2024-07-27 08:25:46 +0200

    Task-URL-Shortener-App Initial commit.

---
+++ src/test/java/com/codefactory/challenge/UrlShortenerApp/UrlShortenerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefactory.challenge.UrlShortenerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
